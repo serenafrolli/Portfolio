3609c7c66fd1cca03f0d10aa76f8682fe21de397
Serena Frolli <serenafrolli2026@u.northwestern.edu>
2025-08-11 16:13:17 +0200

    Update vite.config.js

--- serena-portfolio/vite.config.js
+++ serena-portfolio/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',     // repo name, case-sensitive
+  base: '/Portfolio/', // repo name, case-sensitive
 })
